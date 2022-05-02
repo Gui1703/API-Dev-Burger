ab54fb21af492ade58f8e1368539f8b9678ca202
Guilherme Raposo <gui.raposo36@gmail.com>
2022-05-02 15:38:52 -0300

    feat: created category table

--- src/routes.js
+++ src/routes.js
@@ -5,19 +5,28 @@ import ProductController from "./app/controllers/ProductController"
 import SessionController from "./app/controllers/SessionController"
 import UserController from "./app/controllers/UserController"
 import authMiddleware from "./app/middlewares/auth"
+import CategoryController from "./app/controllers/CategoryController"
 
 const upload = multer(multerConfig)
 
 const routes = new Router()
 
+// Usuário
 routes.post("/users", UserController.store)
 
+// Sessão
 routes.post("/sessions", SessionController.store)
 
 routes.use(authMiddleware) // Será chamado por todas as rotas abaixo
 
+// Produtos
 routes.post("/products", upload.single("file"), ProductController.store)
 
 routes.get("/products", ProductController.index)
+
+// Categorias
+routes.post("/categories", CategoryController.store)
+
+routes.get("/categories", CategoryController.index)
 
 export default routes
