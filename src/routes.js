9293cd54b0c3be406d61b951b9e27fea8669456d
Guilherme Raposo <gui.raposo36@gmail.com>
2022-05-05 15:07:04 -0300

    feat: created order controller

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,7 @@ import SessionController from "./app/controllers/SessionController"
 import UserController from "./app/controllers/UserController"
 import authMiddleware from "./app/middlewares/auth"
 import CategoryController from "./app/controllers/CategoryController"
+import OrderController from "./app/controllers/OrderController"
 
 const upload = multer(multerConfig)
 
@@ -28,5 +29,8 @@ routes.get("/products", ProductController.index)
 routes.post("/categories", CategoryController.store)
 
 routes.get("/categories", CategoryController.index)
+
+// Pedido
+routes.post("/orders", OrderController.store)
 
 export default routes
